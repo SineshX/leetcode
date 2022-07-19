a50e3f43e4f33933a4f632e64f56ec7afd664a78
SineshX <Sineshkumarr@gmail.com>
2022-07-19 10:50:18 +0530

    hint to optimize space

--- 566reshapeMatrix.cpp
+++ 566reshapeMatrix.cpp
@@ -56,8 +56,20 @@ public:
         }
             
         
-//         else
+        // else
         return mat;
+
+        // idea from hints
+        // int m = mat.size(),n=mat[0].size();
+        // if(m*n!=r*c){
+        //      return mat;
+        // }
+        // vector<vector<int>>v(r,vector<int>(c,0));
+        // for(int i=0;i<m*n;i++){
+        //     v[i/c][i%c]=mat[i/n][i%n];
+        // }
+        // return v;
+ 
         
         
     }
