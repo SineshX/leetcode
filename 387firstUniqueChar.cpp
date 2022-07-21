a58f4f78f817035e5d84a10721f738b307922726
SineshX <Sineshkumarr@gmail.com>
2022-07-21 10:49:59 +0530

    Fisrt Unique char in a string

---
+++ 387firstUniqueChar.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<numeric>
+#include<set>
+#include<map>
+#include<unordered_map>
+
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+
+// pasete class solution 
+class Solution {
+public:
+    int firstUniqChar(string s) 
+    {   
+        unordered_map<char, int> map;
+        for(auto i : s)
+        {
+            map[i]++;
+        }
+        for(int i =0 , n = s.size(); i < n; i++)
+        {
+            if (map[s[i]] == 1) return i;
+        }
+        
+        return -1;
+        
+        
+    }
+};
+
+void solve()
+{
+    // take inputs if needed 
+    string s1 = "leetcode";
+    Solution s;
+    cout<<s.firstUniqChar(s1)<<endl;
+}
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ll test=1;
+//cin>>test;
+    while(test--) solve();
+    return 0;
+}
