69858eec0808d0defdfe00f0808169e81cee22d1
SineshX <Sineshkumarr@gmail.com>
2022-07-30 14:21:53 +0530

    BinaryTree Postorder Traversal

---
+++ 145BinaryTreePostorder.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<numeric>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+
+// pasete class solution 
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void postorder(TreeNode* ptr, vector<int> &ans)
+    {
+        if(ptr == nullptr) return;
+
+        postorder(ptr->left, ans);
+        postorder(ptr->right, ans);
+
+        ans.push_back(ptr->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) 
+    {
+        vector<int> ans;
+
+        postorder(root, ans);
+
+
+        return ans;
+    }
+};
+
+void solve()
+{
+    // take inputs if needed 
+    
+    // Solution s;
+    // s.function_name;
+}
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ll test=1;
+//cin>>test;
+    while(test--) solve();
+    return 0;
+}
