94df6f1729708972caab35c77f5f06becf8b03a1
SineshX <Sineshkumarr@gmail.com>
2022-07-30 16:23:17 +0530

    method 2

--- 104BinaryTreeMaxDepth.cpp
+++ 104BinaryTreeMaxDepth.cpp
@@ -27,8 +27,24 @@ struct TreeNode {
 
 class Solution {
 public:
-    
+//m2
     int maxDepth(TreeNode* root) 
+    {
+        if (root == NULL)
+        return 0;
+        
+      int leftDepth = maxDepth(root->left);
+      int rightDepth = maxDepth(root->right);
+
+      // Choose the larger one and add the root to it.
+      if (leftDepth > rightDepth)
+        return leftDepth + 1;
+      else
+        return rightDepth + 1;
+    }
+
+    //m1
+    int maxDepth2(TreeNode* root) 
     {
         if(root == nullptr) return 0;
         
