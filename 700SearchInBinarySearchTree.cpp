0e77003b8bd01b6fdc8d7ee66e767c05470bb38f
SineshX <Sineshkumarr@gmail.com>
2022-08-01 16:30:09 +0530

    BST search

---
+++ 700SearchInBinarySearchTree.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<numeric>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<queue>
+#include<stack>
+
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+
+// paste class solution 
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    // slo 1
+    TreeNode* searchBST(TreeNode* root, int val) 
+    {
+        if(root == nullptr) return NULL;
+        if(root->val == val ) return root;
+        else if(val < root->val )
+        {
+            return searchBST(root->left, val);
+        }
+        else if(val > root->val)
+        {
+            return searchBST(root->right, val);
+        }
+        return NULL;
+    }
+};
+
+void solve()
+{
+    // take inputs if needed 
+    
+    // Solution s;
+    // s.function_name;
+}
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ll test=1;
+//cin>>test;
+    while(test--) solve();
+    return 0;
+}
