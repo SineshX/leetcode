c65bbadea88873a8f37e77d5d53cba5f167a7c87
SineshX <Sineshkumarr@gmail.com>
2022-12-25 21:16:06 +0530

    trapping rain water

---
+++ 42TrappingRainWater.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+
+// paste class solution 
+
+
+void solve()
+{
+    // take inputs if needed 
+    cout<<"hello World"<<endl;
+    // Solution s;
+    // s.function_name;
+}
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ll test=1;
+cin>>test;
+    while(test--) solve();
+
+
+
+    
+    return 0;
+}
